5ab44babd6261911d9f0312a207669165ba34a8f
shishirps7 <sizaleen77@gmail.com>
2025-07-16 12:55:06 +0530

    push3

--- src/components/child.js
+++ src/components/child.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Child = ({change}) => {
+const Child = ({innput,change}) => {
     function handler(e){
         let x=e.target.value
         change(x)
@@ -8,7 +8,7 @@ const Child = ({change}) => {
   return (
     <div style={{display:"flex",flexDirection:'column',gap:"50px",backgroundColor:"red",height:"50%",width:"80%",}}>
        <h2>Child Component</h2>
-        <input type="text" onChange={handler} style={{width:"30%",height:"50px"}}/>
+        <input class="child" type="text" onChange={handler} style={{width:"30%",height:"50px"}} value={innput}/>
     </div>
   )
 }
--- src/components/parent.js
+++ src/components/parent.js
@@ -7,7 +7,7 @@ const Parent = () => {
     <div style={{display:"flex",flexDirection:'column',backgroundColor:"green",height:"500px",width:"800px",padding:"30px"}}>
         <h1>Parent Component</h1>
         <div style={{height:"50px",display:"flex",alignItems:"flex-end"}}>{input}</div>
-        <Child change={setInput}/>
+        <Child change={setInput} innput={input}/>
     </div>
   )
 }
