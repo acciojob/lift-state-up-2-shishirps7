9f11971c75c7de7759125ca8af7d1cffdbae3c0a
shishirps7 <sizaleen77@gmail.com>
2025-07-16 13:39:12 +0530

    push4

--- src/components/child.js
+++ src/components/child.js
@@ -6,9 +6,9 @@ const Child = ({innput,change}) => {
         change(x)
     }
   return (
-    <div style={{display:"flex",flexDirection:'column',gap:"50px",backgroundColor:"red",height:"50%",width:"80%",}}>
+    <div class="child" style={{display:"flex",flexDirection:'column',gap:"50px",backgroundColor:"red",height:"50%",width:"80%",}}>
        <h2>Child Component</h2>
-        <input class="child" type="text" onChange={handler} style={{width:"30%",height:"50px"}} value={innput}/>
+        <input  type="text" onChange={handler} style={{width:"30%",height:"50px"}} value={innput}/>
     </div>
   )
 }
